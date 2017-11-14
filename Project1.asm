de5ec42ef94655c62893182974d08c3cf2ce849b
Brittany Ohalete <brittanyohalete96@gmail.com>
2017-11-14 06:28:34 -0500

    Reading and Checking String Length

--- Project1.asm
+++ Project1.asm
@@ -1,13 +1,52 @@
+
 #DUE NOVEMBER 15 BY MIDNIGHT!!!
+
 .data
- prompt: .ascii "Enter your string"
+prompt: .asciiz "Enter your string: "
+results:   .asciiz "Your string is: "
+invalid: .asciiz "Invalid hexadecimal number."
+
+
+.globl  main
 
 .text
+
+main:
+li $t4, 8 # initialize the length to 8
+
  #prompt users to enter string
- li $v0, 8
+ li $v0, 4
  la $a0, prompt
  syscall
 
- #get users input
- li $v0, 4
+ #read users input
+ li $v0, 8
+ move $a0, $v0
  syscall
+
+jal strlen
+slt $t1,$t4,$v1
+beq $t1, $zero, end
+
+ li $v0, 10
+ syscall
+
+ strlen:
+ addi $t2, $zero, 1 #initialize count to start with 1 for first character
+ j strlen.test
+
+strlen.loop:
+ addi $a1, $a1, 1 #load increment string pointer
+ addi $t2, $t2, 1 #increment count
+strlen.test:
+ lb $t3, 0($a0)   #load the next character to t0
+ bnez $t3, strlen.loop #end loop if null character is reached
+ move $v1, $t0
+
+ jr $ra
+
+end:
+ li $v0, 4
+ la $a0, invalid
+ syscall
+ j EXIT
