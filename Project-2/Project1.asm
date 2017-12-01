0741d41a1904c7717f92dbe7bef1bc32d2fef78e
Brittany Ohalete <brittanyohalete96@gmail.com>
2017-12-01 01:35:45 -0500

    Fixing Subprograms

--- Project-2/Project1.asm
+++ Project-2/Project1.asm
@@ -1,7 +1,7 @@
 .data
 prompt: .asciiz "Enter your string:"
 invalid_string: .asciiz "Invalid hexadecimal number."
-string:   .space 9
+string:   .space 1001
 
 
 .globl  main
@@ -18,28 +18,32 @@ main:
 
  li $t1, 0 # initialize length counter
  la $s0, string # load address of string to s0
- jal loop #call loop to check strings length
- add $t5, $zero, $t1 # store length in t5
- jal validation # call validation function to count sum
+
+
+subprogram_2: # converts single hexadecimal string into a decimal integer
+  jal loop #call loop to check strings length
+  add $t5, $zero, $t1 # store length in t5
+  jal subprogram_1 # call subprogram 1 to validate characters and count sum
 
 
 loop:
- lb $t0, 0($s0) # start at 0th index of string
- beq $t0, 0, exit # exit if index value is null
- beq $t0, 10, exit # exit if index value is new line
- addi $s0, $s0, 1 # increment index
- addi $t1, $t1, 1 # increment length count
- j loop
+  lb $t0, 0($s0) # start at 0th index of string
+  beq $t0, 0, exit # exit if index value is null
+  beq $t0, 10, exit # exit if index value is new line
+  addi $s0, $s0, 1 # increment index
+  addi $t1, $t1, 1 # increment length count
+  j loop
 
 exit:
 la $s0, string
 jr $ra
 
-validation:
+subprogram_1: #checks hexadecimal characters for validation and converts to decimal
  lb $t0, 0($s0)
  addi $s0, $s0, 1
  beq $t0, 0, results
  beq $t0, 10, results
+ beq $t0, 0, exit #if reaches a spaces returns back to string
  # check if char is invalid or num 0-9
  blt $t0, 48, invalid
  blt $t0, 58, is_num
@@ -49,7 +53,7 @@ validation:
  # check if char is invalid or lowercase a-f
  blt $t0, 97, invalid
  blt $t0, 103, is_lower
- j validation
+ j subprogram_1
 
 results:
 slti $t4,$t5, 8 #check is string is greater than length of 7
@@ -90,19 +94,19 @@ invalid:
  syscall
 
 is_upper:
-addiu $t1,$t0,-0x37 # subtract 55 from hex char ('A'- 'F')
-sllv $t2, $t2, 4 # left shift over 4 bits
-add $t2,$t2,$t1 # next char to sum
- j validation
+  addiu $t1,$t0,-0x37 # subtract 55 from hex char ('A'- 'F')
+  sllv $t2, $t2, 4 # left shift over 4 bits
+  add $t2,$t2,$t1 # next char to sum
+  j validation
 
 is_num:
-addiu $t1, $t0, -0x30     # Subtract 48 from hex char ('0'-'9')
-sllv $t2, $t2, 4 # left shift over 4 bits
-add $t2,$t2,$t1 # next char to sum
- j validation
+  addiu $t1, $t0, -0x30     # Subtract 48 from hex char ('0'-'9')
+  sllv $t2, $t2, 4 # left shift over 4 bits
+  add $t2,$t2,$t1 # next char to sum
+  j validation
 
 is_lower:
-addiu $t1, $t0, -0x57     #  subtract 87 from hex char ('a'- 'f')
-sllv $t2, $t2, 4 # left shift over 4 bits
-add $t2,$t2,$t1 # next char to sum
- j validation
+  addiu $t1, $t0, -0x57     #  subtract 87 from hex char ('a'- 'f')
+  sllv $t2, $t2, 4 # left shift over 4 bits
+  add $t2,$t2,$t1 # next char to sum
+  j validation
